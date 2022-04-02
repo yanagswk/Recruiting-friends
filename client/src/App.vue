e9af75b3a3e8906090ececa81728bc256e95abb2
yanagswk <yanagimassu@gmail.com>
2022-04-02 23:34:51 +0900

    fix:整理&アイコンライブラリインストール

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,9 @@
+<script setup lang="ts">
+import DefaultBar from "./layouts/DefaultBar.vue";
+</script>
+
 <template>
-  <nav>
-    <h1 class="font-semibold">こんちは</h1>
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div>
+    <DefaultBar>ダッシュボード</DefaultBar>
+  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
---
+++ client/src/layouts/DefaultBar.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="relative">
+    <h1 class="">hello</h1>
+  </div>
+</template>
